9dd1559dac7a766099acfcfd741c8d2c12c7bb4e
Sadia Khandaker Moumita <sadiakhandakar685@gmail.com>
2026-05-18 13:41:30 +0600

    Add  view, css and js

---
+++ view/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+
+<div class="container">
+    <h2>Login Form</h2>
+
+    <form id="loginForm">
+
+        <label>Email:</label>
+        <input type="text" id="loginEmail" name="email" placeholder="Enter your email">
+        <span class="error" id="loginEmailError"></span>
+
+        <label>Password:</label>
+        <input type="password" id="loginPassword" name="password" placeholder="Enter your password">
+        <span class="error" id="loginPasswordError"></span>
+
+        <button type="submit">Login</button>
+
+    </form>
+
+    <p id="loginMessage"></p>
+
+    <div class="register-link">
+        Don't have an account? <a href="register.php">Register here</a>
+    </div>
+
+</div>
+
+<script src="../controller/ajax/login.js"></script>
+
+</body>
+</html>
